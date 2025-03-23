77bc4e6343cd831d61e278491a7d7a4c0fbfab97
Anurag <anurag95678@gmail.com>
2025-03-23 15:04:42 +0530

    crating a inputbox layout

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,12 +1,18 @@
 import { View, Text, Button, SafeAreaView } from 'react-native'
 import React from 'react'
 import { router } from 'expo-router'
+import { InputBox } from '@/components'
 
 const LoginScreen = () => {
   return (
     <View>
       <SafeAreaView />
       <Text>LoginScreen</Text>
+      {/* <CustomInput
+        placeholder="Enter your email"
+        value={email}
+        setValue={setEmail}
+      /> */}
       <Button title="Register" onPress={() => router.push("/(main-tabs)")} />
     </View>
   )
