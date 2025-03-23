b7f8c12c12cb0e03aeb35fdaf443d5a32d72a817
mohammad shaheer <shaheercse2022@gmail.com>
2025-03-23 19:35:26 +0530

    Created otpInputBox component

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Redirect, router, SplashScreen } from "expo-router";
 import { Logo } from "@/components";
 
+
 const Welcome = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -18,18 +19,20 @@ const Welcome = () => {
   if (isLoggedIn) {
     return <Redirect href={"/(main-tabs)"} />;
   }
-  
+
   return (
     <View className="flex-1 items-center justify-center bg-[#192230] space-y-8">
-      <Logo size={150} />
-      <Pressable 
+      {/* <Logo size={150} />
+      <Pressable
         className="bg-white px-6 py-3 rounded-full"
         onPress={() => router.push("/(auth)")}
       >
         <Text className="text-[#192230] font-semibold text-lg">
           Get Started
         </Text>
-      </Pressable>
+      </Pressable> */}
+
+      
     </View>
   );
 };
