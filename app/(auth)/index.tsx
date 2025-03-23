9f0e3e11bcb10c269c7136ddfa081b8521472f4d
Anurag <anurag95678@gmail.com>
2025-03-23 20:02:10 +0530

    created inputbox component

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,18 +1,41 @@
-import { View, Text, Button, SafeAreaView } from 'react-native'
+import { View, Text, Button, SafeAreaView, } from 'react-native'
 import React from 'react'
 import { router } from 'expo-router'
+import { useState } from 'react'
 import { InputBox } from '@/components'
 
+
 const LoginScreen = () => {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (field: keyof typeof formData, value: string) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+  };
+
+  // console.log(formData,"hii")
+
   return (
     <View>
       <SafeAreaView />
       <Text>LoginScreen</Text>
-      {/* <CustomInput
-        placeholder="Enter your email"
-        value={email}
-        setValue={setEmail}
-      /> */}
+
+      <InputBox
+        label="Display name"
+        value={formData.name}
+        onChangeText={(text) => handleChange("name", text)}
+        message="you can use emoji and special characters"
+      />
+
+      <InputBox
+        label="Username"
+        value={formData.email}
+        onChangeText={(text) => handleChange("email", text)}
+        message="eg:user_007"
+      />
       <Button title="Register" onPress={() => router.push("/(main-tabs)")} />
     </View>
   )
